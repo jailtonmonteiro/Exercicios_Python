03de9e59062b4cc1d667111ba61104f1728fd03b
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-02-18 10:28:48 -0300

    Exercicio 021

---
+++ ex021.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load('E:\Cursos\Python\Exercicios_Python\Arquivos\ex021.mp3')
+pygame.mixer.music.play()
+
+input("Pressione Enter para sair...\n")
