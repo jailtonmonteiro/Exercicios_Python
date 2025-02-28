445c1b0f5378ffb2bd46b19d652c8b8c5a7f87d1
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-02-28 05:41:07 -0300

    Exercicio 054

--- ex054.py
+++ ex054.py
@@ -1,13 +1,13 @@
 from datetime import date
 
-atual = date.today()
+atual = date.today().year
 maior = 0
 menor = 0
 
 for x in range(7):
     ano = int(input('Digite o ano de nascimento da {}ª pessoa: '.format(x+1)))
 
-    if atual.year - ano > 18:
+    if atual - ano > 18:
         maior += 1
     else:
         menor += 1
