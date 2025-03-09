cddc3f37533ba08685d0df371232c653d0d0f820
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-03-09 14:25:08 -0300

    Adição de funcionalide exercicio 068

--- ex068.py
+++ ex068.py
@@ -1,5 +1,7 @@
 from random import randint
 
+vitorias = 0
+
 while True:
     op = str(input('\nPar ou Impar P/I: ')).upper().strip()
     num = int(input('Digite um numero: '))
@@ -10,9 +12,11 @@ while True:
     if soma % 2 == 0 and op == 'P':
         print('Você = {}\nComputador = {}'.format(num, com))
         print('Você ganhou!')
+        vitorias += 1
     elif soma % 2 != 0 and op == 'I':
         print('Você = {}\nComputador = {}'.format(num, com))
         print('Você ganhou!')
+        vitorias += 1
     else:
         print('Você = {}\nComputador = {}'.format(num, com))
         print('Você perdeu')
