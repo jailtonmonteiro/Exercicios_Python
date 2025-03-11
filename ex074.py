b1efe9ded283b892648de376a1d87120a13a1f18
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-03-11 18:18:01 -0300

    Exercicio 074

--- ex074.py
+++ ex074.py
@@ -1,4 +1,18 @@
 from random import randint
 
-numeros = ('{}'.format(randint(0, 999)))
-print(numeros)
+numeros = (randint(0, 999), randint(0, 999), randint(0, 999), randint(0, 999), randint(0, 999))
+
+for n in range(len(numeros)):
+    if n == 0:
+        menor = numeros[n]
+        maior = numeros[n]
+    if menor > numeros[n]:
+        menor = numeros[n]
+    if maior < numeros[n]:
+        maior = numeros[n]
+
+for p in range(len(numeros)):
+    print('{}'.format(numeros[p]), end=' ')
+
+print('\nMaior: {}'.format(maior))
+print('Menor: {}'.format(menor))
